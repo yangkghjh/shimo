91e10aa776a85bf8095349387be7565d04866c51
h4cd <alinxrh@gmail.com>
2021-10-20 20:05:57 +0800

    mei fa xie zhong wen a...

--- config.example.js
+++ config.example.js
@@ -3,5 +3,6 @@ module.exports = {
     Path: 'files', // 存放导出文档的位置
     Folder: '', // 需要导出的文件夹 ID，从浏览器地址栏获取，https://shimo.im/folder/xxx 中 xxx，全部导出则留空
     Recursive: false, // 是否导出子目录
-    Sleep: 1000 // 导出两个文档间的时间间隔，必须设置，否则会服务器忙，单位 ms
+    Sleep: 1000, // 导出两个文档间的时间间隔，必须设置，否则会服务器忙，单位 ms
+    lasttime: 0  //timeship you copy file(run this tool) last time, default 0 means to copy all files this time.
 };
